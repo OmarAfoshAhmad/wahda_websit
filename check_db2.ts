0621912ec952ccdf493850a41669607766c9c91e
omarafosh <eng.omarafosh@gmail.com>
2026-06-11 20:49:44 +0200

    fix deploy card number

--- check_db2.ts
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const ben = await prisma.beneficiary.findUnique({ where: { card_number: 'WAB20258134' } });
-  console.log('BENEFICIARY:', ben);
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
