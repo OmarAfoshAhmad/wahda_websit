13b93275fec7d7bcb4b6e71c6341c8cb72e3e5e4
omarafosh <eng.omarafosh@gmail.com>
2026-06-21 13:29:24 +0200

    fix error import

--- scripts/print-companies.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-async function main() {
-  const c = await prisma.insuranceCompany.findMany({ select: { id: true, name: true, code: true, card_pattern: true } });
-  console.table(c);
-}
-main().finally(() => prisma.$disconnect());
